19294381bbfc98508949d7ef8d63611428503ad8
AbdAlhalemEz <amezzal@lakeheadu.ca>
2025-05-19 16:46:10 -0400

    add

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('tabarak-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/tabarak.txt',
+        '/manifest.json',
+        '/service-worker.js',
+        '/sounds/pop.mp3',
+        '/sounds/done.mp3'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
